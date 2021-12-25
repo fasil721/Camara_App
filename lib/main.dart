f275843d0d72656e75dec76b96660af6f297e6d0
Fasil <fasil17062002@gmail.com>
2021-12-25 15:32:46 +0530

    unnecessary things removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:gallery_saver/gallery_saver.dart';
 
 void main() {
